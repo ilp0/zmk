d5dbb955fb138ad54718ad9ae6b820467f099f5f
ilp0 <ilpo.loikkanen@gmail.com>
2022-04-24 19:04:05 +0000

    progress

--- app/drivers/sensor/azoteq_iqs5xx/iqs5xx.c
+++ app/drivers/sensor/azoteq_iqs5xx/iqs5xx.c
@@ -19,7 +19,7 @@
 #include <devicetree.h>
 
 //DT_DRV_INST(0);
-LOG_MODULE_REGISTER(iqs5xx, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_REGISTER(azoteq_iqs5xx, 1);
 
 /**
  * @brief Read from the iqs550 chip via i2c
@@ -32,7 +32,7 @@ LOG_MODULE_REGISTER(iqs5xx, CONFIG_SENSOR_LOG_LEVEL);
  * @return int 
  */
 static int iqs5xx_seq_read(const struct device *dev, const uint16_t start, uint8_t *read_buf, const uint8_t len) {
-	LOG_DBG("\nseq read iqs5xx");
+	LOG_ERR("\nseq read iqs5xx");
 	const struct iqs5xx_data *data = dev->data;
     const struct iqs5xx_config *config = dev->config;
 	return i2c_write_read(data->i2c, AZOTEQ_IQS5XX_ADDR, &start, sizeof(start), read_buf, len);
@@ -47,11 +47,17 @@ static int iqs5xx_seq_read(const struct device *dev, const uint16_t start, uint8
  * @param len number of bytes to write
  * @return int 
  */
-static int iqs5xx_write(const struct device *dev, const uint16_t start, uint8_t *buf, uint8_t len) {
-	LOG_DBG("\nwrite iqs5xx");
-	const struct iqs5xx_data *data = dev->data;
+static int iqs5xx_write(const struct device *dev, const uint16_t addr, uint8_t *buf, uint8_t len) {
+    LOG_ERR("\nwrite iqs5xx");
+    const struct iqs5xx_data *data = dev->data;
     const struct iqs5xx_config *config = dev->config;
-	return i2c_write_read(data->i2c, AZOTEQ_IQS5XX_ADDR, &start, buf, &start, 1U);
+    // Write address
+    int err = i2c_write(data->i2c, (const uint8_t*)&addr, sizeof(uint16_t), AZOTEQ_IQS5XX_ADDR);
+
+    err |= i2c_write(data->i2c, (const uint8_t*)buf, len, AZOTEQ_IQS5XX_ADDR);
+    LOG_ERR("\n%d", err);
+
+    return err;
 }
 
 /**
@@ -210,29 +216,28 @@ static void iqs5xx_gpio_cb(const struct device *port, struct gpio_callback *cb, 
     const struct device *dev = data->dev;
     k_sem_give(&data->gpio_sem);
 }
-/**
- * @brief TÄÄLLÄ ON HÄIKKÄÄ TODO
- * 
- */
-#define I2C_BUS DT_BUS(DT_NODELABEL(trackpad))
-#define I2C_REG DT_REG_ADDR(DT_NODELABEL(trackpad))
+
+
+//#define I2C_BUS DT_BUS(DT_NODELABEL(trackpad))
+//#define I2C_REG DT_REG_ADDR(DT_NODELABEL(trackpad))
 
 static int iqs5xx_init(const struct device *dev) {
-	LOG_DBG("\nstarting trackpad INIT\n");
+	LOG_ERR("\nstarting trackpad INIT\n");
     struct iqs5xx_data *data = dev->data;
     const struct iqs5xx_config *config = dev->config;
 	uint8_t     activeRefreshRate[2] = {0,8};
 	uint8_t     idleRefreshRate[2] = {0,16};
 	uint8_t		stop = 1; 
-	//init settings
-    iqs5xx_write(dev, IdleRR_adr, &idleRefreshRate[0], 1);
+	/*init settings	*/
+    iqs5xx_write(dev, IdleRR_adr, &idleRefreshRate[0], 2);
 	iqs5xx_write(dev, END_WINDOW, 0, 1);
-	iqs5xx_write(dev, IdleTouchRR_adr, &idleRefreshRate[0], 1);
+	iqs5xx_write(dev, IdleTouchRR_adr, &idleRefreshRate[0], 2);
 	iqs5xx_write(dev, END_WINDOW, 0, 1);
-	iqs5xx_write(dev, LP1RR_adr, &activeRefreshRate[0], 1);
+	iqs5xx_write(dev, LP2RR_adr, &activeRefreshRate[0], 2);
 	iqs5xx_write(dev, END_WINDOW, 0, 1);
-	iqs5xx_write(dev, LP2RR_adr, &activeRefreshRate[0], 1);
+	iqs5xx_write(dev, LP2RR_adr, &activeRefreshRate[0], 2);
 	iqs5xx_write(dev, END_WINDOW, 0, 1);
+
     data->dev = dev;
     gpio_pin_configure(config->dr_port, config->dr_pin, GPIO_INPUT | config->dr_flags);
     gpio_init_callback(&data->gpio_cb, iqs5xx_gpio_cb, BIT(config->dr_pin));
@@ -246,8 +251,7 @@ static int iqs5xx_init(const struct device *dev) {
 
     k_thread_create(&data->thread, data->thread_stack, 1024,
                     (k_thread_entry_t) iqs5xx_thread, (void *) dev, 0, NULL,
-                    K_PRIO_COOP(10), 0, K_NO_WAIT);
-
+                    K_PRIO_COOP(90), 0, K_NO_WAIT);
     return 0;
 }
 
@@ -257,14 +261,15 @@ static const struct sensor_driver_api iqs5xx_driver_api = {
 	.channel_get = iqs5xx_channel_get,
 };
 
+static const struct iqs5xx_data iqs5xx_data = {
+	.i2c = DEVICE_DT_GET(DT_NODELABEL(i2c0)),
+};
+
 static const struct iqs5xx_config iqs5xx_config = {
     .dr_port = DEVICE_DT_GET(DT_GPIO_CTLR(DT_NODELABEL(trackpad), dr_gpios)),
     .dr_pin = DT_INST_GPIO_PIN(0, dr_gpios),
     .dr_flags = DT_INST_GPIO_FLAGS(0, dr_gpios),
 };
 
-static const struct iqs5xx_data iqs5xx_data = {
 
-};
-
- DEVICE_DT_INST_DEFINE(0, iqs5xx_init, device_pm_control_nop, &iqs5xx_data, &iqs5xx_config, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY, &iqs5xx_driver_api);
+DEVICE_DT_INST_DEFINE(0, iqs5xx_init, device_pm_control_nop, &iqs5xx_data, &iqs5xx_config, POST_KERNEL, 20, &iqs5xx_driver_api);
